f674eea60a958f7a06de969c581cac1152360e2d
osamaimran25 <osamaimran25@gmail.com>
2020-06-29 01:07:40 +0500

    new component added

---
+++ src/Dinner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+function FoodServer(props) {
+  
+  return (
+    <div>
+     <h4>Todays we are serving {props.dishName} in our Breakfast </h4>   
+     {/* <h4>Todays we are serving biryani in our dinner </h4>    */}
+     </div>
+  )
+
+
+
+
+}
+
+export default FoodServer;
