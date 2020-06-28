f8e6500d732d6bdb8a3f1ae79edb77b9e9f9f596
osamaimran25 <osamaimran25@gmail.com>
2020-06-29 01:06:54 +0500

    new component add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FoodServer from './Dinner';
+
+
 
 function App() {
+  
   return (
-    <div >
-      {/* <header >
-        Hello All
-        </header> */}
-    <h1>In sha Allah will be best full stack developer Osama  </h1>
-    <p> and will _______ with _________
-
-    </p>
+    <div>
+      Menu is here 
+      <  FoodServer dishName = "biryani"/>
+      <  FoodServer dishName = "Malai boti"/>
     </div>
-  );
+  )
+
+
+
+
 }
 
 export default App;
